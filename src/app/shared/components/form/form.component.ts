8082b9b28f29cc8ba2d0cf039c8d708aa91c65c3
Toydrum <61118847+Toydrum@users.noreply.github.com>
2024-05-18 14:08:26 -0600

    Avance de Formulario genérico

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../input/input.component';
+/* Modules */
+import { SharedModule } from '../../shared.module';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent, SharedModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  userFormGroup: TUser;
 
+  constructor(private formBuilder: FormBuilder){
+    this.userFormGroup = this.formBuilder.group({
+      firstName: this.formBuilder.control(''),
+      lastName: this.formBuilder.control(''),
+      phone: this.formBuilder.control(null),
+      email: this.formBuilder.control(''),
+      userName: this.formBuilder.control(''),
+      password: this.formBuilder.control('')
+    })
+
+  }
+
+  registerUser(){
+
+  }
 }
