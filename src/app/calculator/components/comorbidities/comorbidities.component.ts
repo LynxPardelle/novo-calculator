096bd7ae993d883347c09f67d8f070ebd2ae5b30
Toydrum <61118847+Toydrum@users.noreply.github.com>
2024-09-06 03:45:29 -0600

    style: grosores de inputs

--- src/app/calculator/components/comorbidities/comorbidities.component.ts
+++ src/app/calculator/components/comorbidities/comorbidities.component.ts
@@ -39,9 +39,9 @@ export class ComorbiditiesComponent implements AfterViewInit {
   public falseColors: string =
     'bef bef-d-flex bef-fontWeight-700  bef-bg-HASHf5fafa bef-bw-0 bef-w-auto bef-color-disk bef-h-5rem';
   public filledClasses: string =
-    'bef bef-d-flex jusCon-center aliIte-center  bef-bsb-solid bef-bcb-novo bef-fontWeight-700 bef-rt-0_5px bef-bg-rgbaSD184COM__201COM__214COM__0_2ED   bef-h-2_5rem bef  bef-p-0_5rem';
+    'bef bef-d-flex jusCon-center aliIte-center  bef-bsb-solid bef-bcb-novo bef-bwb-3px bef-fontWeight-700 bef-rt-0_5px bef-bg-rgbaSD184COM__201COM__214COM__0_2ED   bef-h-2_5rem bef  bef-p-0_5rem';
   public emptyClasses: string =
-    'bef bef-d-flex jusCon-center aliIte-center  bef-bsb-solid bef-bcb-novo bef-fontWeight-700 bef-rt-0_5px bef-bg-rgbaSD184COM__201COM__214COM__0_2ED   bef-h-2_5rem bef  bef-p-0_5rem';
+    'bef bef-d-flex jusCon-center aliIte-center  bef-bsb-solid bef-bcb-novo bef-bwb-3px bef-fontWeight-700 bef-rt-0_5px bef-bg-rgbaSD184COM__201COM__214COM__0_2ED   bef-h-2_5rem bef  bef-p-0_5rem';
 
   public thumbsUp = this.sharedService.getHtml('arriba');
   public thumbsDown = this.sharedService.getHtml('abajo');
